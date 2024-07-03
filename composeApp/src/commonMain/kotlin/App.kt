01b50a7484ea9e6d207287638e95bd0070f11e05
Truong Alice <Alice.Truong@ti8m.ch>
2024-07-03 23:16:17 +0200

    init

---
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,142 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        val viewModel: AppViewModel = viewModel { AppViewModel() }
+
+        Scaffold(
+            modifier = Modifier.background(Color(0xf0f2f1)).padding(20.dp),
+            bottomBar = {
+                ReservationBottomBar(
+                    viewModel.resultMessage.collectAsState().value,
+                    viewModel.loading.collectAsState().value,
+                    viewModel.selected.collectAsState().value
+                ) {
+                    CoroutineScope(Dispatchers.Default).launch {
+                        viewModel.confirmReservation()
+                    }
+                }
+            }
+        ) { innerPadding ->
+            Box(modifier = Modifier.padding(innerPadding)) {
+                LazyVerticalGrid(
+                    columns = GridCells.Adaptive(minSize = 120.dp)
+                ) {
+                    val ownerList = viewModel.ownerList.value
+                    items(ownerList.size) { ownerIndex ->
+                        CarOwnerItem(
+                            ownerList[ownerIndex],
+                            viewModel.selected.collectAsState().value == ownerIndex
+                        ) { viewModel.selectItem(ownerIndex) }
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+@Composable
+private fun ReservationBottomBar(
+    resultMessage: String,
+    loading: Boolean,
+    selected: Int?,
+    confirmReservation: () -> Unit
+) {
+    Column {
+        Text(
+            modifier = Modifier.padding(top = 20.dp, bottom = 10.dp),
+            text = resultMessage,
+            maxLines = 1,
+        )
+        Button(
+            modifier = Modifier.fillMaxWidth().height(50.dp),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color(0xFFcae3d5),
+                contentColor = Color.Black
+            ),
+            onClick = {
+                confirmReservation()
+            },
+            enabled = !loading && selected != null,
+            content = {
+                if (loading) {
+                    CircularProgressIndicator(
+                        Modifier.then(Modifier.size(30.dp)),
+                        color = Color(0xFF68ad86)
+                    )
+                } else {
+                    Text("Parking bestätigen")
+                }
+            })
+    }
+}
+
+@Composable
+fun CarOwnerItem(carOwner: CarOwner, selected: Boolean, selectItem: () -> Unit) {
+    Card(
+        backgroundColor = if (selected) Color(0xFFcae3d5) else Color.White,
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .clickable { selectItem() },
+        elevation = 2.dp,
+    ) {
+        Column(modifier = Modifier.padding(all = 16.dp)) {
+            Text(
+                text = carOwner.name,
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp,
+                color = Color.Black,
+                textAlign = TextAlign.Center,
+            )
+            Text(
+                text = carOwner.carType,
+                fontSize = 14.sp,
+                color = Color(0xFF000000),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+            Text(
+                text = "ZH-${carOwner.plateNumber}",
+                fontSize = 14.sp,
+                color = Color(0xFF000000),
+                textAlign = TextAlign.Center,
+            )
+
+        }
+
+    }
+}
+
---
+++ composeApp/src/commonMain/kotlin/AppViewModel.kt
@@ -0,0 +1,65 @@
+import androidx.lifecycle.ViewModel
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class AppViewModel : ViewModel() {
+
+
+    private val _ownerList = MutableStateFlow(listOf<CarOwner>())
+    val ownerList: StateFlow<List<CarOwner>> = _ownerList.asStateFlow()
+
+    private val _selected = MutableStateFlow<Int?>(null)
+    val selected: StateFlow<Int?> = _selected.asStateFlow()
+
+    private val _errorMessage = MutableStateFlow<String>("")
+    val resultMessage: StateFlow<String> = _errorMessage.asStateFlow()
+
+    private val _loading = MutableStateFlow<Boolean>(false)
+    val loading: StateFlow<Boolean> = _loading.asStateFlow()
+
+    init {
+        _ownerList.value = listOf(
+            CarOwner("Roli", "VW Golf", "677929"),
+            CarOwner("Andy", "Ford", "289090"),
+            CarOwner("Jessy", "Mazda", "782035"),
+            CarOwner("Mami", "Audi", "460104"),
+            CarOwner("Coci ", "VW Polo", "191262"),
+            CarOwner("Randy", "", "592006"),
+            CarOwner("Robi", "Toyota", "536428"),
+        )
+    }
+
+    fun selectItem(index: Int) {
+        _selected.value = index
+    }
+
+    suspend fun confirmReservation() {
+        _loading.value = true
+
+        if (selected.value == null) return
+        val client = HttpClient()
+        try {
+            val response = client.get("http://192.168.1.221:8080/parkreservation") {
+                url {
+                    parameters.append("plateNumber", ownerList.value[selected.value!!].plateNumber)
+                }
+            }
+            if (response.status.value in 200..299) {
+                _errorMessage.value = "${ownerList.value[selected.value!!].name} erfolgreich registriert!"
+            } else {
+                _errorMessage.value = "Registrierung fehlgeschlagen!"
+            }
+        } catch (e: Exception) {
+            _errorMessage.value = e.message.toString()
+        }
+
+        _loading.value = false
+
+
+    }
+}
+
+class CarOwner(val name: String, val carType: String, val plateNumber: String) {}
